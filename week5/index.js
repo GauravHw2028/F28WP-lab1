b23106b36d58deaffb3461e0cdc67f07ff77bf0d
Gaurav Chalil <gc2028@hw.ac.uk>
2023-11-23 20:20:48 +0400

    week 5 lab 5 done

--- week5/index.js
+++ week5/index.js
@@ -1,32 +1,39 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './.env' });
 const express = require('express');
 const mysql = require('mysql');
-const dotenv = require('dotenv');
-dotenv.config({path:'./.env'});
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const app = express();
+const port = 5000;
 
-const app=express();
-
-const db=mysql.createConnection({
-    host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE
+const db = mysql.createConnection({
+    host : process.env.DATABASE_HOST,
+    user : process.env.DATABASE_USER,
+    password : process.env.DATABASE_PASSWORD,
+    database : process.env.DATABASE
 });
 
-db.connect((error)=> {
-    if(error){
+
+const publicDirectory = path.join(__dirname, './public');
+app.use(express.static(publicDirectory));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cookieParser());
+app.set('view engine', 'hbs');
+
+db.connect((error) => {
+    if(error) {
         console.log(error);
-    }
-    else{
-        console.log("MySQL connected");
+    } else {
+        console.log('MySQL connected...');
     }
 });
 
-const port =5000;
-app.get("/",(req,res)=> {
-    res.send("<h1>Hello world</h1>");
-});
 
-app.listen(port,()=> {
-    console.log(`Server running on port ${port}`);
+app.use('/', require('./routes/pages'));
+app.use('/auth', require('./routes/auth'));
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}.`); 
 })
-
---
+++ week5/routes/pages.js
@@ -0,0 +1,28 @@
+
+const express=require('express');
+const router=express.Router();
+authController=require('../controllers/auth');
+
+router.get('/',authController.isLoggedIn, (req, res) => {
+    res.render("index", {
+        user: req.user
+    });
+});
+router.get('/register', (req, res) => {
+    res.render("register");
+});
+router.get('/login', (req, res) => {
+    res.render("login");
+});
+router.get('/logout', (req, res) => {
+    res.render("logout");
+});
+router.get('/profile', authController.isLoggedIn, (req, res) => {
+    if(!req.user) {
+        return res.status(401).redirect('/login');
+    }
+    res.render("profile", {
+        user: req.user
+    });
+  });
+module.exports=router;
